574e1b8fefeb5732bd17ddf7ba28267bfa5e7af2
GitHub Action <action@github.com>
2021-08-23 22:28:48 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "Persim_IntelliJ"
--- src/main/kotlin/com/github/sproket/persimintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sproket/persimintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sproket.persimintellij.services
 
+import com.github.sproket.persimintellij.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
